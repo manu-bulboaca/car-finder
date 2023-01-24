f481700a99d0d6a69c760f7b6b4d40a5f79d02c9
Emanuel Bulboaca <Emanuell.Bulboaca@partner.bmw.de>
2023-01-24 14:17:21 +0100

    solution for exercise 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import {FindComponent} from "./find/find.component";
 import {ResultsComponent} from "./results/results.component";
 
-
-const routes: Routes = [
+export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'find' },
   { path: 'find', component: FindComponent },
   { path: 'results', component: ResultsComponent }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
